3d465884de69695ac8ea914ba8890f6c199e5599
harshraj9988 <harshraj9988@gmail.com>
2022-12-11 15:58:37 +0530

    project created

---
+++ app/src/main/java/com/hr9988apps/conduit/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.hr9988apps.conduit.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
